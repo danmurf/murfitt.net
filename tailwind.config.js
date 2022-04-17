0b658537ae90c3b862d64aee4beb46ff643468d8
Dan Murfitt <dan@murfitt.net>
2022-04-17 13:39:33 +0100

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['layouts/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  variants: {
+    extend: {},
+  },
+}
